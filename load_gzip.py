fa2bcfd77823a76ec828134812201dd5c84b94fd
Eero Vilpponen <eero.vilpponen@gmail.com>
2022-04-07 19:34:06 +0300

    improve load

--- load_gzip.py
+++ load_gzip.py
@@ -1,8 +1,9 @@
 import gzip
 import sys
 import csv
-from dateutil import parser
+
 import psycopg2
+import psycopg2.extras
 
 conn = psycopg2.connect(
     host="localhost",
@@ -14,10 +15,28 @@ conn = psycopg2.connect(
 
 filename = sys.argv[1]
 
-users = set()
 data = []
+users = []
 counter = 0
 cur = conn.cursor()
+conn.autocommit = False
+
+
+cur.execute("""
+    SELECT user_name, user_id
+    FROM place_users
+""")
+all_users = {}
+for _users_row in cur.fetchall():
+    all_users[_users_row[0]] = _users_row[1]
+
+print("all_users: {}".format(len(all_users)))
+
+def commit_users(_users):
+    psycopg2.extras.execute_batch(cur, """
+        INSERT INTO place_users(user_name)
+        VALUES (%s) ON CONFLICT DO NOTHING;
+    """, _users)
 
 with gzip.open(filename, mode='rt', compresslevel=9, encoding=None, errors=None, newline=None) as f:
     spamreader = csv.reader(f, delimiter=',', quotechar='"')
@@ -26,22 +45,37 @@ with gzip.open(filename, mode='rt', compresslevel=9, encoding=None, errors=None,
         if first:
             first = False
             continue
-        user = row[1]
-        cur.execute("""
-            INSERT INTO place_users(user_name)
-            VALUES (%s) ON CONFLICT DO NOTHING;
-        """, (user, ))
+        if row[1] in all_users:
+            continue
+        users.append((row[1], ))
+
         counter += 1
-        if counter % 10000 == 0:
-            conn.commit()
+        if counter % 5000 == 0:
+            commit_users(users)
+            users = []
             print(counter)
 
         # if counter > 10000:
         #     conn.commit()
         #     break
 
-counter = 0
+commit_users(users)
+print("done")
+users = []
 
+
+def commit_points(_points):
+    psycopg2.extras.execute_batch(cur, """
+        INSERT INTO points (time, coord_x, coord_y, color, user_id)
+        VALUES (%(date)s, %(x)s, %(y)s, %(color)s, %(user)s)
+        ON CONFLICT DO NOTHING;
+    """, _points)
+
+counter = 0
+data = []
+# import pprofile
+# profiler = pprofile.Profile()
+# with profiler:
 with gzip.open(filename, mode='rt', compresslevel=9, encoding=None, errors=None, newline=None) as f:
     spamreader = csv.reader(f, delimiter=',', quotechar='"')
     first = True
@@ -50,33 +84,30 @@ with gzip.open(filename, mode='rt', compresslevel=9, encoding=None, errors=None,
             first = False
             continue
         user = row[1]
-        users.add(user)
+        #format date, e.g. 2022-04-01 15:38:01.116 UTC
+        _date_str = " ".join(row[0].split(' ')[:-1])
+        if '.' not in _date_str:
+            _date_str += '.000'
+        # date = datetime.datetime.strptime(_date_str, "%Y-%m-%d %H:%M:%S.%f")
         _data = {
-            "date": parser.parse(row[0]),
-            "user": user,
+            "date": _date_str,
+            "user": all_users[user],
             "color": row[2],
             "x": int(row[3].split(',')[0]),
             "y": int(row[3].split(',')[1]),
         }
         data.append(_data)
 
-        cur.execute("""
-            INSERT INTO points (time, coord_x, coord_y, color, user_id)
-            VALUES (%(date)s, %(x)s, %(y)s, %(color)s, (SELECT user_id FROM place_users WHERE user_name = %(user)s))
-            ON CONFLICT DO NOTHING;
-        """, _data)
-
         counter += 1
         if counter % 10000 == 0:
-            conn.commit()
+            commit_points(data)
+            data = []
             print(counter)
 
-        # if counter > 10000:
-        #     conn.commit()
-        #     break
+commit_points(data)
 
+# profiler.dump_stats('profiler_dump.txt')
 
 conn.commit()
 
-print("Users:", len(users))
-print("Data:", len(data))
+print("done")
